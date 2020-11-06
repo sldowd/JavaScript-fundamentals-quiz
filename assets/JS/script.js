3e991acb70258280a80df274dd7c37da9e516b95
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-11-06 08:47:48 -0500

    outlined HTML & pseudocoded JS

---
+++ assets/JS/script.js
@@ -0,0 +1,30 @@
+//create an array of questions 
+
+
+//start game score at 0
+
+
+//start timer when game starts
+
+
+//loop over everyquestion
+    //display questions and answers
+
+
+//compare answers
+
+
+//if correct increase score
+    //show "correct!" under questions
+
+
+//if wrong subtract time
+    //show "Wrong!" under questions
+
+
+//show score at the end of game
+
+
+//give option to restart game
+
+
