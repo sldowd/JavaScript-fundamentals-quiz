a35e6153d2721c25a56d572d5ee42824c4f8c2a4
Sarah Dowd <sarahlynnedowd@gmail.com>
2020-11-06 10:05:32 -0500

    wrote question array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,117 @@
 //create an array of questions 
+var questions = [
+    {
+        q: 'JavaScript code is linked to HTML via which HTML tag?',
+        options: [
+            '<body>',
+            '<head>',
+            '<script>',
+            '<link>'
+        ],
+        answer: '<script>'
+    },
+    {
+        q: 'Which of the following is NOT an example of a primitive data type?',
+        options: [
+            'number',
+            'string',
+            'null',
+            'object'
+        ],
+        answer: 'object'
+    },
+    {
+        q: 'A function must be _____ before it is called.',
+        options: [
+            'concatenated',
+            'defined',
+            'blended',
+            'valued'
+        ],
+        answer: 'defined'
+    },
+    {
+        q: 'In JavaScript an array index begins at ___',
+        options: [
+            'a',
+            'z',
+            '0',
+            '1'
+        ],
+        answer: '0'
+    },
+    {
+        q: 'Values that are passed through parameters into a function are known as _______',
+        options: [
+            'arguments',
+            'callbacks',
+            'statements',
+            'conditionals'
+        ],
+        answer: 'arguments'
+    },
+    {
+        q: 'Which of the following is not a useful tool for fixing bugs in your JavaScript?',
+        options: [
+            'debugger',
+            'Chrome DevTools',
+            'event listener',
+            'console'
+        ],
+        answer: 'event listener'
+    },
+    {
+        q: 'Which of the following is used to observe a user behavior?',
+        options: [
+            'event listener',
+            'for loop',
+            'switch',
+            'variable'
+        ],
+        answer: 'event listener'
+    },
+    {
+        q: 'A variable that is defined within a function has a scope that is described as ________',
+        options: [
+            'global',
+            'functional',
+            'local',
+            'spatial'
+        ],
+        answer: 'local'
+    },
+    {
+        q: 'Which operator could be used to shorten this line of code: n = n + 1?',
+        options: [
+            '+=',
+            '*=',
+            '--',
+            '++'
+        ],
+        answer: '++'
+    },
+    {
+        q: 'DOM is an accronym for:',
+        options: [
+            'Directed Oblong Markup',
+            'Durable Orientation Modules',
+            'Document Object Model',
+            'Dynamic Object Model'
+        ],
+        answer: 'Document Object Model'
+    }
+]
 
-
+// {
+//     title: "Arrays in JavaScript can be used to store ____.",
+//     choices: [
+//       "numbers and strings",
+//       "other arrays",
+//       "booleans",
+//       "all of the above"
+//     ],
+//     answer: "all of the above"
+//   }
 //start game score at 0
 
 
